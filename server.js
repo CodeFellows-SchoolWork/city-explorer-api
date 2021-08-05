baef7ba9abb3ffb6e0bcea330fb236633b55b2a5
jamisonlewin <jamisonlewin@gmail.com>
2021-08-04 20:30:27 -0700

    weather api is working

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 
-let weatherData = require('./data/weather.json');
+const WEATHER = process.env.WEATHER;
+
+// let weatherData = require('./data/weather.json');
+
+const { default: axios } = require('axios');
 
 
 
@@ -26,22 +30,29 @@ function getWeather(request, response) {
   const cityName = request.query.searchQuery;
   const lat = request.query.lat;
   const lon = request.query.lon;
-  const city = (weatherData.find(city => city.city_name.toLowerCase() === cityName.toLowerCase()));
+  // const city = (weatherData.find(city => city.city_name.toLowerCase() === cityName.toLowerCase()));
 
 
-  if (city) {
-    const weatherArr = city.data.map(day => new Forecast(day));
-    response.status(200).send(weatherArr);
+  if (lat && lon) {
+
+    axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${WEATHER}`)
+      .then(result => {
+        let mappedData = [];
+        result.data.data.map(day => {
+          mappedData.push( new Forecast(day.valid_date, day.weather.description))});
+          response.send(mappedData);
+      });
+
+
 
   } else {
-    response.status(500).send('Sorry this city was not found <br /> Please check the spelling of the city and try again.')
+    response.status(500).send('Sorry this city was not found <br /> Please check the spelling of the city and try again.');
   }
-
 };
 
-function Forecast(day) {
-  this.date = day.valid_date
-  this.description = day.weather.description
+function Forecast(date, description) {
+  this.date = date;
+  this.description = description;
 }
 
 
