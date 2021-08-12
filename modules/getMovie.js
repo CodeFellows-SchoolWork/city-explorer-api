0ee5a2fee1ad53e0c5901468152c82ab892c8e02
jamisonlewin <jamisonlewin@gmail.com>
2021-08-12 14:17:44 -0700

    completed lab 10

--- modules/getMovie.js
+++ modules/getMovie.js
@@ -1,26 +1,39 @@
 'use strict';
 
+let cache = require('./cache.js');
+
+require('dotenv').config();
+
 module.exports = getMovie;
 
 const axios = require('axios');
 
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
-function getMovie(request, response) {
+async function getMovie(request, response) {
   const cityName = request.query.searchQuery.split(' ')[0];
+  const key = 'movie-' + cityName;
+  const url = `http://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}`;
 
-  if (cityName) {
-    axios.get(`http://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}`)
-      .then(result => {
-        let mappedData = [];
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+    return cache[key].data;
+  } if (!cache[key]) {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
 
-        const movieToShow = result.data.results.filter(movie => movie.poster_path)
+    let pasCache = await axios.get(url);
+    console.log(pasCache);
+    let mappedData = [];
+    const movieToShow = pasCache.data.results.filter(movie => movie.poster_path)
 
-        movieToShow.map(movie => {
-          mappedData.push(new MovieList(movie))
-        });
-        response.send(mappedData);
-      });
+    movieToShow.map(movie => {
+      mappedData.push(new MovieList(movie))
+    });
+
+    cache[key].data = mappedData;
+    response.send(mappedData);
   } else {
     response.status(500).send('Sorry this city was not found <br /> Please check the spelling of the city and try again.');
   };
